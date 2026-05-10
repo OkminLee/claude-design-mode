5f223a2fe1f524a19bd85b39cecc0eb8ec73341b
okmin <leeokmin@gmail.com>
2026-05-10 20:03:52 +0900

    feat(gh-import): scaffold script + node:test runner

---
+++ skills/design-mode/scripts/__tests__/gh-import.test.js
@@ -0,0 +1,8 @@
+'use strict';
+
+const test = require('node:test');
+const assert = require('node:assert');
+
+test('test runner works', () => {
+  assert.strictEqual(1 + 1, 2);
+});
---
+++ skills/design-mode/scripts/gh-import.js
@@ -0,0 +1,27 @@
+#!/usr/bin/env node
+'use strict';
+
+const path = require('path');
+const fs = require('fs');
+const crypto = require('crypto');
+const { URL } = require('url');
+
+function fail(msg, code) {
+  process.stderr.write(msg + '\n');
+  process.exit(code != null ? code : 1);
+}
+
+function failJson(errorCode, message, exitCode) {
+  process.stdout.write(JSON.stringify({ error: errorCode, message }) + '\n');
+  process.exit(exitCode);
+}
+
+async function main() {
+  fail('not yet implemented');
+}
+
+if (require.main === module) {
+  main().catch(err => fail('unhandled: ' + (err && err.stack || err)));
+} else {
+  module.exports = {};
+}
